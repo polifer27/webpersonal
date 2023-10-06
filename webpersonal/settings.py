9333790c66ad894264c6f55cbc2c02cafb889b08
Pablo Fernández Guembe <fernandezpablo27@yahoo.com.ar>
2023-10-06 11:59:28 -0300

    correccion del debug entre otras cosas

--- webpersonal/settings.py
+++ webpersonal/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-y4xo$#)s_)867r9awv7j3fsb8en7_-w#7t+05=)_lnn%qhfl(6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['paginapersonal.pythonanywhere.com']
 
